eacbce79250fde5afa27286aa2560dc101e59733
root <nicolasjonason@gmail.com>
2023-01-28 23:32:22 +0000

    use ordered dict

---
+++ note_dataset.py
@@ -0,0 +1,97 @@
+import torch
+import numpy as np
+from collections import OrderedDict
+
+def onehot(idx,n_values):
+    onehot = np.zeros(n_values)
+    onehot[idx]=1
+    return onehot
+
+def noteseq_to_model_format(note_sequence, n_pitches,n_timesteps, sequence_length):
+    """
+    Converts a note sequence to a model input format.
+    """
+    note_type=[]
+    note_pitch=[]
+    note_onset=[]
+    note_duration=[]
+    for step_index in range(sequence_length):
+      
+        if step_index < len(note_sequence):
+            # type section
+            note_type.append(onehot(0,2))
+            # pitch section
+            note_pitch.append(onehot(note_sequence[step_index]["pitch"],n_pitches))
+            # onset section
+            note_onset.append(onehot(note_sequence[step_index]["start"],n_timesteps))
+            # duration section
+            duration = note_sequence[step_index]["end"]-note_sequence[step_index]["start"]
+            note_duration.append(onehot(duration, n_timesteps))
+        else:
+            note_type.append(onehot(1,2))
+            # uniform distribution
+            note_pitch.append(np.ones(n_pitches)/n_pitches)
+            note_onset.append(np.ones(n_timesteps)/n_timesteps)
+            note_duration.append(np.ones(n_timesteps)/n_timesteps)
+    seq = OrderedDict()
+    seq["type"] = np.stack(note_type)
+    seq["pitch"] = np.stack(note_pitch)
+    seq["onset"] = np.stack(note_onset)
+    seq["duration"] = np.stack(note_duration)
+    return seq
+
+class NoteDataset(torch.utils.data.Dataset):
+    def __init__(self, prepared_data_path, crop_size=None):
+        self.crop_size = crop_size
+        self.load_data(prepared_data_path)
+        # filter away noteseq of length 0
+        self.data = [example for example in self.data if len(example["note_seq"]) > 0]
+        
+
+    def load_data(self, path):
+        self.data = torch.load(path)
+
+    def save_data(self, path):
+        torch.save(self.data,path)
+
+    def get_token_sections(self):
+        n_pitches=36
+        n_timesteps=64
+        return [
+            {"label":"type","n_channels":2},
+            {"label":"pitch","n_channels":n_pitches},
+            {"label":"onset","n_channels":n_timesteps},
+            {"label":"duration","n_channels":n_timesteps}
+            ]
+
+
+    def __getitem__(self, idx):
+        example = self.data[idx]
+        note_seq = example["note_seq"].copy()
+
+        # get min and max pitch
+        min_pitch = np.min([note["pitch"] for note in note_seq])
+        max_pitch = np.max([note["pitch"] for note in note_seq])
+
+        low = min(max_pitch-self.crop_size,min_pitch)
+        high = max(max_pitch-self.crop_size,min_pitch)
+
+        if low==high:
+            start_pitch = low
+        else:
+            start_pitch = np.random.randint(low=low,high=high)
+     
+        end_pitch = start_pitch+self.crop_size
+
+        # remove all notes outside of range and shift pitch to start at 0
+        note_seq = [note for note in note_seq if note["pitch"] >= start_pitch and note["pitch"] < end_pitch]
+        for note in note_seq:
+            note["pitch"] -= start_pitch
+
+        seq = noteseq_to_model_format(note_seq,36,64,128)
+        
+        return {"seq":seq}
+        
+
+    def __len__(self):
+        return len(self.data)
--- note_train.py
+++ note_train.py
@@ -1,5 +1,5 @@
 #%%
-from midi_dataset import NoteSeqDataset
+from note_dataset import NoteDataset
 
 import numpy as np
 import torch
@@ -17,87 +17,112 @@ from torch.nn import functional as F
 from note_model import TransformerModel
 import glob
 import datetime
+from collections import OrderedDict
+
 # make generic so that it accepts a graph with dependencies between the various events/sections
 # also rewrite with better use of tensor operations.
 # recursive perhaps?
 
-def special_loss(logits,target,mask,token_sections=[{"label":"null","n_channels":1}]):
+# split sections : splits a b s t sequence into a sequence of sections
+
+# merge sections
+
+
+def special_loss(logits,target,mask):
+    merged_target = merge_channels(target)
+    merged_mask = merge_channels(mask)
+    merged_logits = merge_channels(logits)
+
     loss=0
-    masked_target = target*(1-mask)-mask
+    merged_masked_target = merged_target*(1-merged_mask)-merged_mask
+
     # matrix of shape (batch_size, pred_timesteps, target_timesteps) 
     # where each element is 1 if the masked target is equal and 0 otherwise
-    masked_target_is_equal = (masked_target.unsqueeze(1)==masked_target.unsqueeze(2)).all(dim=-1)
-    target_sum = target[:,None,:, :]*masked_target_is_equal[...,None]
-    target_mean = torch.mean(target_sum,axis=1)
-
-    channel_offset=0
-
-    logits = einops.rearrange(logits, "batch sequence channel -> (batch sequence) channel")
-    target_mean=einops.rearrange(target_mean, "batch sequence channel -> (batch sequence) channel")
-    mask = einops.rearrange(mask, "batch sequence channel -> (batch sequence) channel")
-
-    for i in range(len(token_sections)):
-        section_loss = torch.nn.functional.cross_entropy(
-            logits[...,channel_offset:token_sections[i]["n_channels"]+channel_offset],
-        target_mean[...,channel_offset:token_sections[i]["n_channels"]+channel_offset],
-       reduction="none")
-        section_loss = section_loss * (torch.sum(mask[...,channel_offset:channel_offset+token_sections[i]["n_channels"]],axis=-1)==token_sections[i]["n_channels"])
-        loss+=torch.mean(section_loss)
-        channel_offset += token_sections[i]["n_channels"]
+    masked_target_is_equal = (merged_masked_target.unsqueeze(1)==merged_masked_target.unsqueeze(2)).all(dim=-1)
+    batch_size ,n_timesteps, _ = merged_logits.shape
+    for channel in logits.keys():
+        expanded_target = target[channel][:,None,...]*masked_target_is_equal[...,None]
+        target_mean = torch.mean(expanded_target,dim=2)
+        channel_loss = torch.nn.functional.cross_entropy(
+            logits[channel],
+            target_mean, 
+            reduction="none",
+        )
+        is_masked = mask[channel].sum(dim=-1)==mask[channel].shape[-1]
+        loss+=torch.mean(channel_loss*is_masked[:,None])
     return loss
-      
+
+def merge_channels(seq, total_size=None):
+    if total_size is None:
+        total_size = sum([value.shape[-1] for key, value in seq.items()])
+    batch_size, n_timesteps,_ = next(iter(seq.items()))[1].shape
+    seq_device = next(iter(seq.items()))[1].device
+    merged_seq = torch.zeros((batch_size,n_timesteps,total_size),device=seq_device)
+    offset=0
+ 
+    for key, value in seq.items():
+        merged_seq[...,offset:offset+value.shape[-1]] = seq[key]
+        offset+=value.shape[-1]
+    return merged_seq
+
+def split_channels(seq, token_sections):
+    split_seq = OrderedDict()
+    offset=0
+    for token_section in token_sections:
+        split_seq[token_section["label"]] = seq[...,offset:offset+token_section["n_channels"]]
+        offset+=token_section["n_channels"]
+    return split_seq
 
 class Model(pl.LightningModule):
     def __init__(self, token_sections, n_layers=None,n_hidden_size=None):
         super().__init__()
 
         self.token_sections = token_sections
-        self.n_channels = sum([section["n_channels"] for section in self.token_sections])
+        self.total_size = sum([section["n_channels"] for section in self.token_sections])
 
-        self.model = TransformerModel(n_channels=self.n_channels,n_layers=n_layers,n_hidden_size=n_hidden_size)
+        self.model = TransformerModel(n_channels=self.total_size,n_layers=n_layers,n_hidden_size=n_hidden_size)
 
     def forward(self,x,mask):
         y = self.model(x,mask)
         return y
     
     def compute_loss(self, logits, targets, mask):
-        return special_loss(logits,targets,mask, self.token_sections)
+        return special_loss(logits,targets,mask)
 
     def training_step(self, batch, batch_idx):
-
         # get data
         seq = batch["seq"]
-        # batch size
-        batch_size = seq.shape[0]
 
-        n_sections = len(self.token_sections)
+        # batch size
+        batch_size, sequence_length, _  = next(iter(seq.items()))[1].shape
+        device = next(iter(seq.items()))[1].device
+
+        n_sections = len(seq)
 
         # compute mask ratio
-        mask_ratio = self.schedule(torch.rand(batch_size,device=seq.device))
-        mask_ratio = einops.repeat(mask_ratio,'b -> b sequence sections',sequence=seq.shape[1],sections=n_sections)
+        mask_ratio = self.schedule(torch.rand(batch_size,device=device))
+        mask_ratio = einops.repeat(mask_ratio,'b -> b sequence sections',sequence=sequence_length,sections=n_sections)
         # compute mask
         section_mask = (torch.rand(mask_ratio.shape,device=mask_ratio.device) < mask_ratio).float()
 
-        channel_offset=0
-        mask = []
-        for i in range(n_sections):
-            mask.append(section_mask[:,:,i].unsqueeze(-1).repeat(1,1,self.token_sections[i]["n_channels"]))
-        mask = torch.concat(mask,axis=-1)
-
-        assert mask.shape == seq.shape 
+        mask = OrderedDict()
+        for i, key, value in zip(range(n_sections),seq.keys(),seq.values()): 
+            mask[key]=section_mask[...,i].unsqueeze(-1).expand(value.shape)
         
+        merged_seq = merge_channels(seq,self.total_size)
+        merged_mask = merge_channels(mask,self.total_size)
         # compute loss
-        y = self(seq,mask)
-
+        
+        y = self(merged_seq,merged_mask)
         # time iteration
         timea = datetime.datetime.now()
 
-        loss = self.compute_loss(y, seq,mask)
+        logits = split_channels(y,self.token_sections)
 
+
+        loss = self.compute_loss(logits,seq,mask)
         timeb = datetime.datetime.now()
-
         # print(timeb-timea)
-
         # log loss
         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
@@ -113,36 +138,38 @@ class Model(pl.LightningModule):
             mask_ratio = torch.tensor(mask_ratio,device=self.device)
         return torch.acos(mask_ratio)/(np.pi/2.0)
 
-    # def generate(self, n_sampling_steps, temperature=1.0, x= None,mask=None, activity_bias=0, use_confidence_sampling=True, plot=False):
-    #     if x is None:
-    #         x = torch.zeros((1,self.n_pitches, self.n_timesteps,self.vocab_size),device=self.device)
-    #         x[:,:,:,1] = 1
-    #     else:
-    #         x  = piano_roll_to_model_format(x)
-    
-    #     if mask is None:
-    #         mask = torch.ones((1,self.n_pitches, self.n_timesteps,1),device=self.device)
-    #     else:
-    #         mask = (mask)[...,None]
 
-    #     mask_ratio = torch.mean(mask,dim=[1,2])
+
+    # def generate(self,x=None, section_mask=None, n_sampling_steps=None, temperature=1.0, activity_bias=0, plot=False):
+
+    #     total_n_channels = sum([section["n_channels"] for section in self.token_sections])
+
+    #     if x is None:
+    #         x = torch.zeros((1,self.n_timesteps,total_n_channels),device=self.device)
+    #     if section_mask is None:
+    #         section_mask = torch.ones((1,self.n_timesteps,total_n_channels),device=self.device)
+
+    #     mask_ratio = torch.mean(section_mask,dim=[1,2])
     #     step = int(self.inverse_schedule(mask_ratio)*n_sampling_steps)
 
-    #     # Inference algo from https://arxiv.org/pdf/2202.04200.pdf
+    #     batch_size ,n_timesteps, n_channels = x.shape
+
     #     with torch.no_grad():
 
     #         for step in range(step,n_sampling_steps):
 
+    #             mask = torch.cat([section_mask[:,:,i].unsqueeze(-1).repeat(1,1,self.token_sections[i]["n_channels"]) for i in range(len(self.token_sections))],axis=-1)
+
     #             y,_ = self(x,mask)
 
-    #             y = einops.rearrange(y,'b p t c -> b (p t) c')
+    #             y = einops.rearrange(y,'b t c -> b t c')
 
     #             y[:,:,0] += activity_bias
 
     #             probs = torch.softmax(y/temperature,dim=-1)
 
     #             # n tokens to mask
-    #             n_mask = int(np.floor(self.schedule((step+1)/n_sampling_steps).cpu()*self.n_pitches * self.n_timesteps))
+    #             n_mask = int(np.floor(self.schedule((step+1)/n_sampling_steps).cpu()*n_timesteps*self.n_sections))
 
     #             # sample from probs
     #             sampled_indices = torch.distributions.Categorical(probs=probs).sample()
@@ -275,6 +302,8 @@ class Model(pl.LightningModule):
 
     #     assert torch.all(torch.sum(x,axis=-1) == 1)
     #     return model_format_to_piano_roll(x)
+
+
     
     def configure_optimizers(self):
         return self.model.configure_optimizers()
@@ -290,13 +319,13 @@ if __name__ == "__main__":
     np.random.seed(0)
 
 
-    ds = NoteSeqDataset(prepared_data_path="data/prepared_gamer_noteseq_data.pt", crop_size=36)
+    ds = NoteDataset(prepared_data_path="data/prepared_gamer_noteseq_data.pt", crop_size=36)
 
     dl = torch.utils.data.DataLoader(ds, batch_size=512, shuffle=True, num_workers=20)
 
     wandb_logger = WandbLogger()
     
-    trainer = pl.Trainer(logger=wandb_logger, callbacks=[],log_every_n_steps=1,gpus=[0])
+    trainer = pl.Trainer(logger=wandb_logger, callbacks=[],log_every_n_steps=1,gpus=[1])
     model = Model(token_sections=ds.get_token_sections(),n_layers=4,n_hidden_size=512)
 
 
